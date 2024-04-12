365ceb41c7d8fa0a6ccdd8b13f7d2d0fc4dc8892
Rouven Himmelstein <r.himmelstein@tarent.de>
2024-04-12 15:34:44 +0200

    faet: improve pipe

---
+++ run.sh
@@ -0,0 +1,8 @@
+#!/bin/env bash
+
+export BW_CLIENT_MODS_DIR=/home/rouven/projects/beammp-server-beiwagen/client-mods
+export BW_MODS=13061
+export BW_OUTDATED=skip
+export BW_UNSUPPORTED=delete
+
+cargo run
